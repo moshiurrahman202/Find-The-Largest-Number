087882c443d09968ecd136420e0fcaf439effe0b
Mosiur Rahman <mosiurrahman123654@gmail.com>
2022-03-26 20:32:30 +0600

    noramal if statement

---
+++ index.js
@@ -0,0 +1,13 @@
+var n1 = prompt("Enter First Number: ");
+var n2 = prompt("Enter Second Number: ");
+var n3 = prompt("Enter Third Numder:" );
+
+n1 = parseInt(n1);
+n2 = parseInt(n2);
+n3 = parseInt(n3);
+
+if(n1 >= n2 && n1 >= n3){
+    console.log(n1 + " n1 is the largest number!");
+} else if( n2 >= n1 && n2 >= n3){
+    console.log(n2 + " n2 is the largest number!");
+}else{console.log(n3 + " n3 is the larget numder!");}
