b73c49eea4010f32821dc642ac2427fe7ee63d40
Mosiur Rahman <mosiurrahman123654@gmail.com>
2022-03-27 15:17:47 +0600

    Nested id statement

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ n1 = parseInt(n1);
 n2 = parseInt(n2);
 n3 = parseInt(n3);
 
-if(n1 >= n2 && n1 >= n3){
-    console.log(n1 + " n1 is the largest number!");
-} else if( n2 >= n1 && n2 >= n3){
-    console.log(n2 + " n2 is the largest number!");
-}else{console.log(n3 + " n3 is the larget numder!");}
+// if(n1 >= n2 && n1 >= n3){
+//     console.log(n1 + " n1 is the largest number!");
+// } else if( n2 >= n1 && n2 >= n3){
+//     console.log(n2 + " n2 is the largest number!");
+// }else{console.log(n3 + " n3 is the larget numder!");}
+
+if(n1 >= n2){
+    if(n1 >= n3){
+        console.log(n1 + " n1 is the largest number!");
+    } else{ console.log(n3 + " n3 is the largest number!");}
+}else{
+    if(n2 >= n3){
+        console.log(n2 + " n3 is the largest number!");
+    } else{
+        console.log(n3 + " n3 is the largest number!");
+    }
+}
